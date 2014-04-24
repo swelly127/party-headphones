de79a110f6b57a2ffe90f470ab3ec09fd7c43a2e
arh97 <arh97@cornell.edu>
2014-04-23 22:14:35 -0400

    Navigation bar
    This is the navigation bar, depending on what the client wants can be required on any page. All fields are subject to change.

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<div id="navigation">
+	<ul>
+		<li><a href="about.html">About</a></li>
+			<ul>
+				<li><a href="faqs.html">FAQs</a></li>
+			</ul>	
+		<li><a href="contact.php">Contact</a></li>
+		<li><a href="order.php">Order</a></li>
+	</ul>
+</div>	
