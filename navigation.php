a90d60f33c4f7da5ed70ed960e1e1711d728f729
swelly127 <jessicashu127@gmail.com>
2014-04-29 00:47:32 -0400

    change login location

--- navigation.php
+++ navigation.php
@@ -2,15 +2,6 @@
   require_once 'config.php';
   include 'session.php';
   $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-  if (isset($_SESSION['username'])) {
-    header("Location: portal.php");
-  }
-
-  if (isset($_POST["login"])) {
-  	// handle login and redirect to portal.php if sucessful. otherwise set $error to and error msg
-    header("Location: portal.php");
-  }
 ?>
 
 <div id="navigation">
